cabb36525c27801c5d4b40b52420c52224cd083a
gerson <gerson.barros@m2n.com.br>
2020-07-17 16:35:50 -0300

    Axios para requisições

---
+++ src/components/TarefasLista.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-sm-10">
+                <h1 class="font-weight-light">Lista de tarefas</h1>
+            </div>
+            <div class="col-sm-2">
+                <button @click="exibirFormulario = !exibirFormulario" class="btn btn-primary float-right">
+                    <i class="fa fa-plus mr-2"></i>
+                    <span>Criar</span>
+                </button>
+            </div>
+        </div>
+
+        <ul class="list-group" v-if="tarefas.length > 0">
+            <TarefasListaIten
+                v-for="tarefa in tarefas"
+                :key="tarefa.id"
+                :tarefa="tarefa" />
+        </ul>
+
+        <p v-else>Nenhuma tarefa criada.</p>
+
+        <TarefaSalvar v-if="exibirFormulario"
+        @criar="criarTarefa" />
+
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+import config from './../config/config'
+
+import TarefaSalvar from './TarefaSalvar.vue'
+import TarefasListaIten from './TarefasListaIten.vue'
+
+export default {
+    components: {
+        TarefaSalvar,
+        TarefasListaIten
+    },
+    data() {
+        return {
+            tarefas: [],
+            exibirFormulario: false,
+        }
+    },
+    created() {
+        axios
+        .get(`${config.apiURL}/tarefas`)
+        .then((response) => {
+            this.tarefas = response.data
+        })
+    },
+    methods: {
+        criarTarefa(tarefa) {
+            axios
+            .post(`${config.apiURL}/tarefas`, tarefa)
+            .then((response)=> {
+                this.tarefas.push(response.data)
+                this.exibirFormulario = false
+            })
+        }
+    },
+}
+</script>
